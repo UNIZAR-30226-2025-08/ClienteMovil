ff0d024da854e84b3424d4541dfafd6b63de1ee7
Marcosis2 <869021@unizar.es>
2025-04-13 13:14:44 +0200

    Mejora textos de Pantalla ¿Cómo jugar?

--- cliente-movil/app/(comoJugar)/(turnos)/turnoDia.tsx
+++ cliente-movil/app/(comoJugar)/(turnos)/turnoDia.tsx
@@ -1,22 +1,22 @@
-import React from 'react';  // Importar useState desde React
-import { 
-  ImageBackground, 
-  StyleSheet, 
-  Text, 
-  View, 
-  Image, 
-  TouchableOpacity
-} from 'react-native';
-import { useRouter } from 'expo-router';
-import { useFonts } from 'expo-font';
+import React from "react"; // Importar useState desde React
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { useFonts } from "expo-font";
 
 /**
  * Importación de imágenes utilizadas en la pantalla del turno de día.
  */
-const imagenFondoRoles = require('@/assets/images/fondo-roles.jpg');
-const imagenSol = require('@/assets/images/imagen-sol.jpg');
-const imagenPapiro = require('@/assets/images/papiro.png')
-const imagenAtras = require('@/assets/images/botonAtras.png');
+const imagenFondoRoles = require("@/assets/images/fondo-roles.jpg");
+const imagenSol = require("@/assets/images/imagen-sol.jpg");
+const imagenPapiro = require("@/assets/images/papiro.png");
+const imagenAtras = require("@/assets/images/botonAtras.png");
 
 /**
  * Pantalla del turno de día en el juego.
@@ -25,12 +25,11 @@ const imagenAtras = require('@/assets/images/botonAtras.png');
  * @returns {JSX.Element} Pantalla de explicación del turno de día.
  */
 export default function TurnoDiaScreen(): JSX.Element | null {
-
-  const router = useRouter();  // Usamos useRouter para manejar la navegación
+  const router = useRouter(); // Usamos useRouter para manejar la navegación
 
   // Cargar la fuente GhostShadow
   const [loaded] = useFonts({
-    GhostShadow: require('@/assets/fonts/ghost-shadow.ttf'),
+    GhostShadow: require("@/assets/fonts/ghost-shadow.ttf"),
   });
 
   if (!loaded) {
@@ -41,40 +40,35 @@ export default function TurnoDiaScreen(): JSX.Element | null {
    * Función para regresar a la pantalla anterior.
    */
   const irAtras = () => {
-    router.back();  // Regresa a la pantalla anterior
+    router.back(); // Regresa a la pantalla anterior
   };
 
   return (
     <View style={styles.container}>
       <ImageBackground
         source={imagenFondoRoles}
-        resizeMode='cover'
+        resizeMode="cover"
         style={styles.image}
       >
+        <View style={styles.overlay} />
+        <Text style={styles.tituloTurnoDia}>DIA</Text>
+        <Image source={imagenSol} style={styles.imageSol}></Image>
+        <Image source={imagenPapiro} style={styles.imagePapiro}></Image>
+        <Text style={styles.textoTurnoDia}>
+          Para empezar, se conocerá qué jugador murió la noche anterior.
+          Posteriormente, los jugadores tendrán un tiempo de hablar para
+          discutir quienes pueden ser los hombres lobos. Tras debatir, votarán a
+          un jugador a su elección para ser linchado (el jugador con más votos
+          será eliminado de la partida y se mostrará públicamente su rol). Las
+          fases de noche y día se alternan sucesivamente hasta que únicamente
+          queden hombres lobo o aldeanos vivos y la partida termine con la
+          victoria de uno de los dos bandos.
+        </Text>
 
-      <View style={styles.overlay} />
-      <Text style={styles.tituloTurnoDia}>DIA</Text>
-      <Image source={imagenSol} style={styles.imageSol}></Image>
-      <Image source={imagenPapiro} style={styles.imagePapiro}></Image>
-      <Text style={styles.textoTurnoDia}>
-        DIA (todos los jugadores tienen los ojos abiertos y hablan, 
-        tratando de encontrar a los hombres lobo, mientras los hombres 
-        lobo se hace pasar por aldeanos).
-        El narrador indicará quién ha muerto durante la noche anterior. 
-        Durante la fase del día, todos los jugadores intentarán descubrir quién(es) 
-        entre ellos es un hombre lobo y tras debatir, votarán a un jugador a su 
-        elección para ser linchado (el jugador con más votos será eliminado de 
-        la partida y mostrará públicamente su carta).
-        Las fases de noche y día se alternan sucesivamente hasta que únicamente 
-        queden hombres lobo o aldeanos vivos y la partida termine con la 
-        victoria de uno de los dos bandos.
-      </Text>
-
-      {/* Botón de regreso */}
-      <TouchableOpacity style={styles.containerAtras} onPress={irAtras}>
-            <Image source={imagenAtras} style={styles.imageAtras} />
-      </TouchableOpacity>
-
+        {/* Botón de regreso */}
+        <TouchableOpacity style={styles.containerAtras} onPress={irAtras}>
+          <Image source={imagenAtras} style={styles.imageAtras} />
+        </TouchableOpacity>
       </ImageBackground>
     </View>
   );
@@ -86,14 +80,13 @@ export default function TurnoDiaScreen(): JSX.Element | null {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
 
   containerAtras: {
-    position: 'absolute',
-    bottom: 20,  
-    left: '46%',  
-
+    position: "absolute",
+    bottom: 20,
+    left: "46%",
   },
 
   imageAtras: {
@@ -102,53 +95,53 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 
   imagePapiro: {
     height: 400,
     width: 300,
-    position: 'absolute',
+    position: "absolute",
     bottom: "10%",
     left: "13%",
   },
 
   overlay: {
-    ...StyleSheet.absoluteFillObject,  
-    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
   },
 
   imageSol: {
-    width: 170,  
-    height: 170, 
+    width: 170,
+    height: 170,
     left: "30%",
     top: "15%",
-    position: 'absolute',
+    position: "absolute",
     borderRadius: 100,
   },
 
   tituloTurnoDia: {
-    position: 'absolute',  
-    top: '5%',  
-    left: '57%',  
-    marginTop: 20,  
-    marginLeft: -60, 
-    color: 'white',  
-    fontSize: 45, 
-    fontWeight: 'bold',  
-    textAlign: 'center',  
+    position: "absolute",
+    top: "5%",
+    left: "57%",
+    marginTop: 20,
+    marginLeft: -60,
+    color: "white",
+    fontSize: 45,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 
   textoTurnoDia: {
     fontSize: 10.5,
-    fontWeight: 'bold',
-    position: 'absolute',
+    fontWeight: "bold",
+    position: "absolute",
     width: 230,
     left: "25%",
-    bottom: "30%"
-  }
+    top: "45%",
+  },
 });
--- cliente-movil/app/(comoJugar)/(turnos)/turnoNoche.tsx
+++ cliente-movil/app/(comoJugar)/(turnos)/turnoNoche.tsx
@@ -1,21 +1,22 @@
-import React from 'react';  // Importar useState desde React
-import { 
-  ImageBackground, 
-  StyleSheet, 
-  Text, 
-  View, 
-  Image, 
-  TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
-import { useFonts } from 'expo-font';
+import React from "react"; // Importar useState desde React
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { useFonts } from "expo-font";
 
 /**
  * Importación de imágenes utilizadas en la pantalla del turno de noche.
  */
-const imagenFondoRoles = require('@/assets/images/fondo-roles.jpg');
-const imagenLuna = require('@/assets/images/imagen-luna.png');
-const imagenPapiro = require('@/assets/images/papiro.png')
-const imagenAtras = require('@/assets/images/botonAtras.png');
+const imagenFondoRoles = require("@/assets/images/fondo-roles.jpg");
+const imagenLuna = require("@/assets/images/imagen-luna.png");
+const imagenPapiro = require("@/assets/images/papiro.png");
+const imagenAtras = require("@/assets/images/botonAtras.png");
 
 /**
  * Pantalla del turno de noche en el juego.
@@ -24,12 +25,11 @@ const imagenAtras = require('@/assets/images/botonAtras.png');
  * @returns {JSX.Element} Pantalla explicativa del turno de noche.
  */
 export default function TurnoNocheScreen(): JSX.Element | null {
-
-  const router = useRouter();  // Usamos useRouter para manejar la navegación
+  const router = useRouter(); // Usamos useRouter para manejar la navegación
 
   // Cargar la fuente GhostShadow
   const [loaded] = useFonts({
-    GhostShadow: require('@/assets/fonts/ghost-shadow.ttf'),
+    GhostShadow: require("@/assets/fonts/ghost-shadow.ttf"),
   });
 
   if (!loaded) {
@@ -40,19 +40,19 @@ export default function TurnoNocheScreen(): JSX.Element | null {
    * Función para regresar a la pantalla anterior.
    */
   const irAtras = () => {
-    router.back();  // Regresa a la pantalla anterior
+    router.back(); // Regresa a la pantalla anterior
   };
 
   return (
     <View style={styles.container}>
       <ImageBackground
         source={imagenFondoRoles}
-        resizeMode='cover'
+        resizeMode="cover"
         style={styles.image}
       >
         <View style={styles.overlay} />
 
-      {/* Título de la pantalla */}
+        {/* Título de la pantalla */}
         <Text style={styles.tituloTurnoNoche}>NOCHE</Text>
 
         {/* Imagen representativa del turno de noche */}
@@ -63,26 +63,27 @@ export default function TurnoNocheScreen(): JSX.Element | null {
 
         {/* Descripción de la fase nocturna */}
         <Text style={styles.textoTurnoNoche}>
-          Todos los jugadores tienen los ojos cerrados, salvo aquellos 
-          que van siendo llamados por el narrador y actúan en silencio.
-          Orden de actuar durante la noche: vidente, hombres lobo, bruja.
-          Vidente: elige un jugador y el Narrador le mostrará la carta de dicho jugador.
-          Hombres Lobo: los hombres lobo abrirán los ojos para elegir a su próxima víctima, 
-          cuya muerte se hará efectiva y anunciada en el próximo turno de día. Si no hay 
-          unanimidad en la designación de la víctima, no hay muerte, por lo que dispondrán 
-          de un tiempo limitado para elegir a su víctima (a discreción del Narrador).
-          Bruja: El Narrador le señala el jugador que está a punto de morir en esa noche 
-          (si lo hay) y ésta decidirá qué hacer. Puede salvarlo, 
-          gastando su Poción de la Vida, o dejarlo morir. A continuación, 
-          la bruja tiene la opción de gastar su Poción de la Muerte para eliminar 
-          al jugador que desee. Ambas pociones son de solo un uso durante la partida.
-          En caso de que suceda la muerte del Cazador: El Narrador le comunicará que ha 
-          sido asesinado y el cazador podrá eliminar a un jugador de su elección.
+          Durante la noche los personajes con roles especiales van despertando y
+          realizan diversas acciones. Orden de actuar durante la noche: vidente,
+          hombres lobo, bruja. Para empezar, la vidente elige un jugador y el
+          Narrador le mostrará el rol de dicho jugador. Luego, los hombres lobo
+          aparecerán para elegir a su próxima víctima, cuya muerte se hará
+          efectiva y anunciada en el próximo turno de día. Si no hay unanimidad
+          en la designación de la víctima, no hay muerte, por lo que dispondrán
+          de un tiempo limitado para elegir a su víctima. Para continuar, la
+          bruja podrá observar qué jugador está a punto de morir en esa noche
+          (si lo hay) y decidirá si desea salvarlo gastando su Poción de la
+          Vida, o dejarlo morir. A continuación, la bruja tiene la opción de
+          gastar su Poción de la Muerte para eliminar al jugador que desee.
+          Ambas pociones son de solo un uso durante la partida. Y por último, en
+          caso de que suceda la muerte del cazador: Cazador: Se le comunicará
+          que ha sido asesinado y el cazador podrá eliminar a un jugador de su
+          elección.
         </Text>
 
         {/* Botón para regresar a la pantalla anterior */}
         <TouchableOpacity style={styles.containerAtras} onPress={irAtras}>
-              <Image source={imagenAtras} style={styles.imageAtras} />
+          <Image source={imagenAtras} style={styles.imageAtras} />
         </TouchableOpacity>
       </ImageBackground>
     </View>
@@ -95,14 +96,13 @@ export default function TurnoNocheScreen(): JSX.Element | null {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
 
   containerAtras: {
-    position: 'absolute',
-    bottom: 20,  
-    left: '46%', 
-
+    position: "absolute",
+    bottom: 20,
+    left: "46%",
   },
 
   imageAtras: {
@@ -111,53 +111,53 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
   },
 
   imagePapiro: {
     height: 400,
     width: 300,
-    position: 'absolute',
+    position: "absolute",
     bottom: "10%",
     left: "13%",
   },
 
   overlay: {
-    ...StyleSheet.absoluteFillObject,  
-    backgroundColor: 'rgba(0, 0, 0, 0.4)', 
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
   },
 
   imageLuna: {
-    width: 170,  
+    width: 170,
     height: 170,
     left: "30%",
     top: "15%",
-    position: 'absolute',
+    position: "absolute",
     borderRadius: 100,
   },
 
   tituloTurnoNoche: {
-    position: 'absolute',  
-    top: '5%',  
-    left: '47%',  
-    marginTop: 20,  
-    marginLeft: -60,  
-    color: 'white', 
-    fontSize: 45, 
-    fontWeight: 'bold', 
-    textAlign: 'center',  
+    position: "absolute",
+    top: "5%",
+    left: "47%",
+    marginTop: 20,
+    marginLeft: -60,
+    color: "white",
+    fontSize: 45,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 
   textoTurnoNoche: {
     fontSize: 10.5,
-    fontWeight: 'bold',
-    position: 'absolute',
+    fontWeight: "bold",
+    position: "absolute",
     width: 230,
     left: "25%",
-    bottom: "18%"
-  }
+    top: "45%",
+  },
 });
