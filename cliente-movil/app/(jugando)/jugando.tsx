09caf37fb5f292447f0d7b669bee8e671823a94e
Alberto Francisco Solaz García <873373@unizar.es>
2025-05-03 15:41:56 +0200

    Emergencia vidente

--- cliente-movil/app/(jugando)/jugando.tsx
+++ cliente-movil/app/(jugando)/jugando.tsx
@@ -2074,6 +2074,8 @@ const Jugando: React.FC = () => {
       : false;
   }, [jugadoresEstado, indiceUsuario]);
 
+  const [visionJugador, setVisionJugador] = useState("");
+
   // ---------------------------------------------------------------------------
   // Encolar el evento correspondiente cuando el usuario local ha muerto
   // ---------------------------------------------------------------------------
@@ -2374,24 +2376,7 @@ const Jugando: React.FC = () => {
           `Evento recibido: visionJugador - ${JSON.stringify(data)}`,
           jugadoresEstado[indiceUsuario]
         );
-
-        const regex = /El jugador (.+) es (.+)\./;
-        const match = data.mensaje.match(regex);
-      
-        if (match) {
-          const idObjetivo = match[1];
-          const rolObjetivo = match[2];
-      
-          setIdObjetivoVidente(idObjetivo);
-          setRolObjetivoVidente(rolObjetivo);
-      
-          logCustom(
-            jornadaActual,
-            etapaActual,
-            `Jugador ${idObjetivo} tiene el rol ${rolObjetivo}`,
-            jugadoresEstado[indiceUsuario]
-          );
-        }
+        setVisionJugador(data);
       });
     socket.on("jugadorSalido", (data) => {
       logCustom(
@@ -3030,8 +3015,8 @@ const Jugando: React.FC = () => {
             opacity={opacitiesEjecutarHabilidadVidente[0]}
             mostrarComponente={mostrarComponentesEjecutarHabilidadVidente[0]}
             texto={
-              rolObjetivoVidente !== ""
-                ? `EL jugador ${idObjetivoVidente.toUpperCase()} ES ${rolObjetivoVidente.toUpperCase()}.` // TODO
+              visionJugador !== ""
+                ? visionJugador
                 : "NO HAS SELECCIONADO A UN JUGADOR AL QUE VISUALIZAR"
             }
           />
